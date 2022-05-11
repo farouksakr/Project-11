64c986f3a655341405df8df3d3673269eb914723
farouk sakr <farouksakr>
2022-05-11 22:05:15 +0200

    new one

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(function () {
         upperH = $('.upper-bar').innerHeight(),
         navH = $('.navbar').innerHeight();
 
-    $('.slider, .carousel-item').height( winH - (upperH + navH) );
+    $('.slider, .carousel-item').height(winH - (upperH + navH));
 
     // featured work shuffle
 
@@ -14,17 +14,43 @@ $(function () {
 
         $(this).addClass('active').siblings().removeClass('active');
 
-        if ( $(this).data('class') === 'all' ) {
+        if ($(this).data('class') === 'all') {
 
             $('.shuffle-images .col-md').css('opacity', 1)
-            
+
         } else {
 
             $('.shuffle-images .col-md').css('opacity', '.08')
-            $( $(this).data('class') ).parent().css('opacity', 1)
+            $($(this).data('class')).parent().css('opacity', 1)
 
         }
 
     });
+
+    // scroll to top
+
+    var scrollButton = $('#scroll-top')
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() >= 1000) {
+
+            scrollButton.fadeIn(1000)
+
+        } else {
+
+            scrollButton.fadeOut(1000);
+
+        }
+
+    });
+
+    // click on button to scroll top
+
+    scrollButton.click(function () {
+
+        $('html, body').animate({ scrollTop: 0 }, 1000)
+
+    })
 
 });
