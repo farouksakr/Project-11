5f198c705cdf4b94d0d1bd5e4913d92e7d6d5a31
farouk sakr <farouksakr>
2022-05-11 22:39:29 +0200

    new-one

--- js/main.js
+++ js/main.js
@@ -10,22 +10,22 @@ $(function () {
 
     // featured work shuffle
 
-    $('.featured-work ul li ').on('click', function () {
+    // $('.featured-work ul li ').on('click', function () {
 
-        $(this).addClass('active').siblings().removeClass('active');
+    //     $(this).addClass('active').siblings().removeClass('active');
 
-        if ($(this).data('class') === 'all') {
+    //     if ($(this).data('class') === 'all') {
 
-            $('.shuffle-images .col-md').css('opacity', 1)
+    //         $('.shuffle-images .col-md').css('opacity', 1)
 
-        } else {
+    //     } else {
 
-            $('.shuffle-images .col-md').css('opacity', '.08')
-            $($(this).data('class')).parent().css('opacity', 1)
+    //         $('.shuffle-images .col-md').css('opacity', '.08')
+    //         $($(this).data('class')).parent().css('opacity', 1)
 
-        }
+    //     }
 
-    });
+    // });
 
     // scroll to top
 
